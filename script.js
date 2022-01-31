e33981633c4b1b9f2b2dbd96aebc24fbfb66c20a
Jason <jasonkirk555@gmail.com>
2022-01-31 14:54:35 +0000

    added player factory functions

--- script.js
+++ script.js
@@ -1,56 +1,61 @@
 const gameBoard = (()=>{
-
-  let _gameBoard = [1,2,3,4,5,6,7,8,9];
-  
+  let _gameBoard = [1,2,3,4,5,6,7,8,9];  
   const setBoard = (index, value) => {
       _gameBoard[index]= value;
      return _gameBoard
-  }
-  
+  }  
+
+  const makeBoard = ()=>{
+    let board=document.querySelector(".board");
+    for (i=0;i<gameBoard.setBoard().length;i++){    
+    createNewBox(i);
+    clickBox(i); 
+    }
+    
+    function createNewBox(boxNumber) {
+      let newDiv = document.createElement('div');
+      newDiv.classList="newDiv";
+      newDiv.setAttribute('id', "box"+boxNumber);
+      newDiv.innerText="box "+ gameBoard.setBoard()[boxNumber];
+      board.appendChild(newDiv);     
+    }
+    
+    function clickBox(boxNumber){
+    let foo = document.querySelector("#box"+boxNumber);
+      console.log(foo);
+      foo.addEventListener('click', ()=>{
+        alert("this is box"+boxNumber);
+      })
+    }
+
+  };
   
   return {
-    //  _gameBoard,
-      setBoard
-  }
+      setBoard,
+      makeBoard
+  }  
+  })();  
   
-  })();
-  
-  
-  gameBoard.setBoard(0,"X");
-  
-  gameBoard.setBoard(1,"O");  
-  
-  function makeBoard(array) {
-     let board=document.querySelector(".board");
+  // gameBoard.setBoard(0,"X");  
+  // gameBoard.setBoard(1,"O");  
+  // console.log(gameBoard.makeBoard());
 
-for (i=0;i<gameBoard.setBoard().length;i++){
 
-createNewBox(i);
-}
+  const Player = (name, marker) => {
+    return {
+      name,
+      marker
+    }
+  };
 
-function createNewBox(boxNumber) {
-  let newDiv = document.createElement('div');
-  newDiv.classList="newDiv";
-  newDiv.setAttribute('id', "box"+boxNumber);
-  newDiv.innerText="box "+ gameBoard.setBoard()[boxNumber];
-  board.appendChild(newDiv);
-  clickBox(boxNumber); 
-}
+  const player1= Player("Player1", "X");
+  const player2= Player("Player2", "O");
 
-function clickBox(boxNumber){
-  //let foo = document.getElementsByClassName("newDiv");
- let foo = document.querySelector("#box"+boxNumber);
-  console.log(foo);
-  foo.addEventListener('click', ()=>{
-    alert("this is box"+boxNumber);
-  })
-}
-  }
+   gameBoard.setBoard(0,"X"); 
+   gameBoard.setBoard(0, player2.marker);
+    gameBoard.makeBoard();
 
- 
 
-  
-  makeBoard();
-  
 
-  
+  console.log(player2.name);
+
