61758cd89626c584cb1031f0ea24ab26ce336eaf
Jason <jasonkirk555@gmail.com>
2022-02-05 18:55:22 +0000

    just playing around with te check win function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const gameBoard = (()=>{
-  let _gameBoard = [1,2,3,4,5,6,7,8,9];  
- 
+   let _gameBoard = [1,2,3,4,5,6,7,8,9];  
+    
+
   const makeBoard = ()=>{
     let board=document.querySelector(".board");
 
@@ -16,20 +17,6 @@ const gameBoard = (()=>{
     }
   };
 
- /* const makeBoard = ()=>{
-    let board=document.querySelector(".board");
-    for (i=0;i<_gameBoard.length;i++){    
-    createNewBox(i);
-  //  clickBox(i); 
-    }    
-      function createNewBox(boxNumber) {
-      let newDiv = document.createElement('div');
-      newDiv.classList="newDiv";
-      newDiv.setAttribute('id', boxNumber);
-      newDiv.innerText="box "+ +(boxNumber+1);
-      board.appendChild(newDiv);     
-    }
-  };*/
 
 const getBoard = ()=>{
   return _gameBoard;
@@ -42,14 +29,11 @@ return false;
 }
 
 else {
-
 _gameBoard[box]=player.marker;
 let divId =document.querySelector(`div[id="${box}"]`);
 divId.innerText=player.marker;
-console.log(`value is ${player.marker}`);
+
 runGame.switchPlayer();
-console.log(_gameBoard);
-return true;
 }
   };
 
@@ -62,16 +46,16 @@ return true;
 
   const makeBoardInteractive = (()=>{
     const clickBox = (player) => {
-      let fooPlayer = player;
+      let thisPlayer = player;
       let boardBoxes = document.getElementsByClassName("newDiv");
  
       Array.from(boardBoxes).forEach((element)=>{
   
-       element.addEventListener('click', (e)=>{         
-     
-        gameBoard.setBoard(element.id, fooPlayer);
-        
-                 fooPlayer = currrentPlayer;                   
+       element.addEventListener('click', ()=>{     
+       
+             gameBoard.setBoard(element.id, thisPlayer); 
+             runGame.checkWins();         
+                 thisPlayer = currrentPlayer;                   
            });
       });   
     };
@@ -94,18 +78,62 @@ return true;
     let player2 = Player("player2", "O");
     let currrentPlayer;
 
-  const runGame = (()=>{       
-      
-  function switchPlayer(){     
-   if (currrentPlayer==player1){
-     currrentPlayer = player2;
-   }
-  else if (currrentPlayer==player2){
-    currrentPlayer = player1;
-  }
-      } 
+  const runGame = (()=>{   
+
+    const switchPlayer = ()=>{
+      if (currrentPlayer==player1){
+        currrentPlayer = player2;
+      }
+     else if (currrentPlayer==player2){
+       currrentPlayer = player1;
+     }
+    };
+
+    const checkWins = ()=>{
+     
+     // console.log("gameboard is "+gameBoard.getBoard());
+      fooBoard = gameBoard.getBoard();
+      let getEachInstanceOfWinningMoves;
+
+     let winningMoves = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
+       
+     function findEachInstanceOf1() {
+      console.log(`wining moves is ${winningMoves}`);
+      //console.log(winningMoves.every(val => val === winningMoves[0]));
+ 
+let fooMoves;
+      winningMoves.forEach(element => {
+        console.log(`winningmoves[0][0] is ${winningMoves[0][0]}`);
+
+        const index = element.indexOf(1);
+        console.log("index is: "+ index);
+        element[index] = "X";
+      });
+
+      console.log()
+
+    }
+
+
+    findEachInstanceOf1();
+     
+     
+     // console.log("winning moves is "+winningMoves[0]);
+
+      //foo=["X","X","X"];
+     // console.log(`foo[0] is ${foo[0]}`);
+     // console.log(foo.every(val => val === foo[0]));
+
+     //console.log(array1.every(isBelowThreshold));
+
+      //winningMoves.forEach(element => console.log(element));
+    };
+
+   
+
   return {
-    switchPlayer
+    switchPlayer,
+    checkWins
   }  
   })();
 
